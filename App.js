2c799989543cd74fdf65a48256bccb6c9bb891ee
khomabhena <colwanymab@gmail.com>
2023-11-25 18:16:36 +0200

    Added the database

--- App.js
+++ App.js
@@ -1,65 +1,120 @@
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
-import { useEffect, useState } from 'react';
-import { validateEmail } from './utils';
-import OnBoarding from './screens/OnBoarding';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Profile from './screens/Profile';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import SplashScreen from './screens/SplashScreen';
-import Home from './screens/Home';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect, useMemo, useReducer, useCallback } from "react";
+import { Alert } from "react-native";
+// import Onboarding from "./screens/Onboarding";
+import Onboarding from './screens/OnBoarding'
+import Profile from "./screens/Profile";
+import SplashScreen from "./screens/SplashScreen";
+import Home from "./screens/Home";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
-  const Stack = createNativeStackNavigator()
-  const [loggedIn, setLoggedIn] = useState(true)
-  const [isLoading, setIsLoading] = useState(true)
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { AuthContext } from "./contexts/AuthContext";
+
+const Stack = createNativeStackNavigator();
+
+export default function App({ navigation }) {
+  const [state, dispatch] = useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case "onboard":
+          return {
+            ...prevState,
+            isLoading: false,
+            isOnboardingCompleted: action.isOnboardingCompleted,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isOnboardingCompleted: false,
+    }
+  );
 
   useEffect(() => {
-      (async () => {
-        try {
-          const state = await AsyncStorage.getItem('loggedIn')
-          // const value = JSON.parse(state) === 'true'
-
-          // setLoggedIn(value)
-        } catch (error) {
-          Alert.alert("Error: " + error.message)
-        } finally {
-          setIsLoading(false)
+    (async () => {
+      let profileData = [];
+      try {
+        const getProfile = await AsyncStorage.getItem("profile");
+        if (getProfile !== null) {
+          profileData = getProfile;
         }
-      })()
-  }, [])
+      } catch (e) {
+        console.error(e);
+      } finally {
+        if (Object.keys(profileData).length != 0) {
+          dispatch({ type: "onboard", isOnboardingCompleted: true });
+        } else {
+          dispatch({ type: "onboard", isOnboardingCompleted: false });
+        }
+      }
+    })();
+  }, []);
 
-  if (isLoading)
-    return <SplashScreen />
+  const authContext = useMemo(
+    () => ({
+      onboard: async data => {
+        try {
+          const jsonValue = JSON.stringify(data);
+          await AsyncStorage.setItem("profile", jsonValue);
+        } catch (e) {
+          console.error(e);
+        }
+
+        dispatch({ type: "onboard", isOnboardingCompleted: true });
+      },
+      update: async data => {
+        try {
+          const jsonValue = JSON.stringify(data);
+          await AsyncStorage.setItem("profile", jsonValue);
+        } catch (e) {
+          console.error(e);
+        }
+
+        Alert.alert("Success", "Successfully saved changes!");
+      },
+      logout: async () => {
+        try {
+          await AsyncStorage.clear();
+        } catch (e) {
+          console.error(e);
+        }
+
+        dispatch({ type: "onboard", isOnboardingCompleted: false });
+      },
+    }),
+    []
+  );
+
+  if (state.isLoading) {
+    return <SplashScreen />;
+  }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {loggedIn ? (
+    <AuthContext.Provider value={authContext}>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {state.isOnboardingCompleted ? (
             <>
-              <Stack.Screen name="Home" component={Home} />
-              <Stack.Screen name='Profile' component={Profile} />
+              <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name="Profile" component={Profile} />
             </>
-            ) : (
-              <Stack.Screen name='Welcome' component={OnBoarding} />
-            )
-        }
-      </Stack.Navigator>
-    </NavigationContainer>
+          ) : (
+            <Stack.Screen
+              name="Onboarding"
+              component={Onboarding}
+              options={{ headerShown: false }}
+            />
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 24,
-  },
-  title: {
-    marginTop: 32,
-    color: "#333333",
-    textAlign: "center",
-    fontSize: 32,
-  },
-});
